702355b9c6c5532af2d057fd0112f7c00aca7590
Kevin Jackman <notyper@gmail.com>
2023-05-17 17:56:47 -0400

    first commit

---
+++ js/game.js
@@ -0,0 +1,27 @@
+function return_choice (selected){
+    switch (selected){
+        case "1": return "Rock";
+        break;
+        case "2": return "Paper";
+        break;
+        case "3": return "Scissors";
+        break;
+        default: return "invalid";
+    }
+}
+
+function getComputerChoice(){
+    choice = Math.floor(Math.random() * 3) +1;
+    console.log("compnumber ", choice)
+}
+console.log('In the game');
+// print opening message
+console.log("welcome to the game");
+// prompt for choice
+console.log("1=Rock, 2=Paper, 3=Scissors");
+choice = prompt( "please choose 1, 2, or 3\n1=Rock, 2=Paper, 3=Scissors");
+console.log("you chose: ", return_choice(choice));
+// read choice
+// generate random computer choice
+// determine winner
+// output winner
