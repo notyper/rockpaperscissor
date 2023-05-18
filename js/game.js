b0d2f6b8e6e68e81ab8dfff60019a75c0f823e78
Kevin Jackman <notyper@gmail.com>
2023-05-17 21:46:58 -0400

    Move game.js file to correct folder

--- js/game.js
+++ js/game.js
@@ -11,16 +11,36 @@ function return_choice (selected){
 }
 
 function getComputerChoice(){
-    choice = Math.floor(Math.random() * 3) +1;
-    console.log("compnumber ", choice)
+    let comp_choice = Math.floor(Math.random() * 3) +1;
+    console.log("computer number ", comp_choice);
+    return comp_choice;
 }
-console.log('In the game');
+function playRound(playerSelection, computerSelection) {
+    let winner = "";
+    if (playerSelection === computerSelection) {
+        winner = "It is a tie";
+    } else if ((playerSelection === "Rock" && computerSelection === "Paper") 
+            || (playerSelection === "Paper" && computerSelection === "Rock")
+            || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        winner = "Player is the winner";
+    } else {
+        winner = "Computer is the winner"}
+    return winner;
+    // return(`Player: ${playerSelection} : Computer ${computerSelection}`)
+  }
+   
+
 // print opening message
 console.log("welcome to the game");
 // prompt for choice
 console.log("1=Rock, 2=Paper, 3=Scissors");
 choice = prompt( "please choose 1, 2, or 3\n1=Rock, 2=Paper, 3=Scissors");
 console.log("you chose: ", return_choice(choice));
+compChoice = getComputerChoice().toString();
+console.log("The computer chose: ",  return_choice(compChoice))
+
+winner = playRound (return_choice(choice), return_choice(compChoice));
+console.log(`Winner : ${winner}`);
 // read choice
 // generate random computer choice
 // determine winner
